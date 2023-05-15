4737fc7b19ac974ad6896df0927f20c3ad6826a5
Mert <mertcansarginn@gmail.com>
2023-05-16 01:09:48 +0300

    160523

---
+++ 16. Çarpım tablosu uygulaması/app.js
@@ -0,0 +1,7 @@
+for(let say1=1;say1<=10;say1++){
+    for(let say2=1;say2<=10;say2++){
+        console.log(say1+"x"+say2+"="+say1*say2);
+    }
+    console.log("-------------------------------");
+}
+
---
+++ 17. Faktöryel Bulma Programı/app.js
@@ -0,0 +1,6 @@
+let sayi=Number(prompt("Sayı giriniz: "));
+ let fakt=1;
+for(let i=1;i<=sayi;i++){
+   fakt=sayi*fakt;
+    
+}alert(fakt);
